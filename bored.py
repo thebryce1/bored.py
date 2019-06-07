7a159f3c6a649f3b193095c324652c6a91ac888c
thebryce1 <34398740+thebryce1@users.noreply.github.com>
2019-06-07 14:24:18 -0500

    Add files via upload

---
+++ bored.py
@@ -0,0 +1,141 @@
+import time
+
+# This is how fucking bored I am in Iowa
+
+# Intro text
+print("Welcome to Storm Lake, IA")
+time.sleep(3)
+print("Home of nothing to do")
+time.sleep(3)
+print("So to pass some time I wrote this to say...")
+time.sleep(1)
+print(" 'Im so bored' ... in 30 major languages")
+time.sleep(2)
+# Input for language selection
+print("Choose language to translate to...")
+print("1. Arabic")
+print("2. Armenian")
+print("3. Burmese")
+print("4. Czech")
+print("5. English")
+print("6. Farsi (Persian)")
+print("7. French")
+print("8. German")
+print("9. Gujarati")
+print("10. Hindi")
+print("11. Italian")
+print("12. Japanese")
+print("13. Javanese")
+print("14. Kannada")
+print("15. Korean")
+print("16. Malay, Indonesian")
+print("17. Marathi")
+print("18. Polish")
+print("19. Portuguese")
+print("20. Punjabi")
+print("21. Russian")
+print("22. Spanish")
+print("23. Tamil")
+print("24. Telugu")
+print("25. Thai")
+print("26. Turkish")
+print("27. Urdu")
+print("28. Vietnamese")
+print("29. Zulu")
+print("30. All (Best Option")
+# End of language list
+
+lang = input(str("Enter number of language selection: "))
+
+# selction of translation
+if lang == "1":
+    print("اشعر بلملل")
+elif lang == "2":
+    print("Այնպես եմ ձանձրանում")
+elif lang == "3":
+    print("dar kyountpyinn im")
+elif lang == "4":
+    print("Tolik se nudím")
+elif lang == "5":
+    print("Im so bored")
+elif lang == "6":
+    print("من خیلی خسته ام")
+elif lang == "7":
+    print("Je m'ennuie tellement")
+elif lang == "8":
+    print("Mir ist so langweilig")
+elif lang == "9":
+    print("હું કંટાળો આવ્યો છું")
+elif lang == "10":
+    print("मैं ऊब चूका हूँ")
+elif lang == "11":
+    print("Sono così annoiato")
+elif lang == "12":
+    print("私はとても退屈")
+elif lang == "13":
+    print("Im dadi bosen")
+elif lang == "14":
+    print("ನನಗೆ ತುಂಬಾ ಬೇಸರವಾಗಿದೆ")
+elif lang == "16":
+    print("너무 심심해.")
+elif lang == "17":
+    print("Saya sangat bosan")
+elif lang == "18":
+    print("मला खूप कंटाळा आला आहे")
+elif lang == "19":
+    print("Jestem taki znudzony")
+elif lang == "20":
+    print("Estou tão entediado")
+elif lang == "21":
+    print("ਮੈਂ ਇੰਨਾ ਬੋਰ ਹੋਇਆ")
+elif lang == "22":
+    print("Мне так скучно")
+elif lang == "23":
+    print("Estoy tan aburrido")
+elif lang == "24":
+    print("நான் மிகவும் சலிப்படைந்தேன்")
+elif lang == "25":
+    print("నేను విసుగు చెందాను")
+elif lang == "26":
+    print("ฉันเบื่อมาก")
+elif lang == "27":
+    print("Çok sıkıldım")
+elif lang == "28":
+    print("میں بہت بور ہوا")
+elif lang == "29":
+    print("Tôi chán quá")
+elif lang == "30":
+    print("Ngiyithukuthele kangaka")
+else:   
+    print('''اشعر بلملل
+Այնպես եմ ձանձրանում
+dar kyountpyinn im
+Tolik se nudím
+Im so bored
+من خیلی خسته ام
+Je m'ennuie tellement
+Mir ist so langweilig
+હું કંટાળો આવ્યો છું
+मैं ऊब चूका हूँ
+Sono così annoiato
+私はとても退屈
+Im dadi bosen
+ನನಗೆ ತುಂಬಾ ಬೇಸರವಾಗಿದೆ
+너무 심심해.
+Saya sangat bosan
+मला खूप कंटाळा आला आहे
+Jestem taki znudzony
+Estou tão entediado
+ਮੈਂ ਇੰਨਾ ਬੋਰ ਹੋਇਆ
+Мне так скучно
+Estoy tan aburrido
+நான் மிகவும் சலிப்படைந்தேன்
+నేను విసుగు చెందాను
+ฉันเบื่อมาก
+Çok sıkıldım
+میں بہت بور ہوا
+Tôi chán quá
+Ngiyithukuthele kangaka''')
+# End of Translate
+
+# Start Program
